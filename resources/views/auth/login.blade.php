41d6b44835e3db1344408f047c0235b2a856b5c5
ihsan fauzi <muhammadihsan10.mifr@gmail.com>
2023-01-15 23:14:31 +0700

    update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>SEMNASTEK UNSUR</title>
 
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
     <style>
         .gradient {
             background: linear-gradient(90deg, #36368d 0%, #343563 100%);
@@ -19,16 +20,26 @@
         <div class="flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
           <!-- form -->
           <div class="md:w-1/2 px-8 md:px-16">
-            <h2 class="font-bold text-2xl text-gray-600">Login</h2>
-            <p class="text-xs mt-4 text-gray-600">If you are already a member, easily log in</p>
-
-            <form action="{{route('login')}}" class="flex flex-col gap-4" method="POST">
+            <h2 class="font-bold text-2xl text-center text-gray-600">Login</h2>
+            <form action="{{route('login')}}" method="POST">
                 @csrf
-              <input class="p-2 mt-8 rounded-xl border" type="email" name="email" placeholder="Email">
-              <div class="relative">
-                <input class="p-2 rounded-xl border w-full" type="password" name="password" placeholder="Password">
-              </div>
-              <button type="submit" class="gradient rounded-xl text-white py-2 hover:scale-105 duration-300">Login</button>
+                <div class="mt-8">
+                    <div class="text-sm font-bold text-gray-700 tracking-wide">Email Address</div>
+                    <input class="w-full text-sm py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500 bg-transparent" type="" placeholder="Masukan email yang terdaftar">
+                </div>
+                <div class="mt-2">
+                    <div class="flex justify-between items-center">
+                        <div class="text-sm font-bold text-gray-700 tracking-wide">
+                            Password
+                        </div>
+                    </div>
+                    <input class="w-full text-sm py-2 border-b border-gray-300 focus:outline-none focus:border-blue-800 bg-transparent" type="password" placeholder="Masukkan password yang sesuai">
+                </div>
+                <div class="mt-10">
+                    <button class="gradient text-gray-100 px-4 py-2 w-full rounded-lg tracking-wide font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-indigo-600 shadow-lg">
+                        Log In
+                    </button>
+                </div>
             </form>
 
             <div class="mt-6 grid grid-cols-3 items-center text-gray-400">
