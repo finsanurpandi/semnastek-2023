2cb9da46d1e6feac1875b077226c65ab576f7ea1
ihsan fauzi <muhammadihsan10.mifr@gmail.com>
2023-03-22 18:26:02 +0700

    resolve missing code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,7 @@
                         <div class="nav-item">
                             <a class="nav-link" href="{{ route('editor.article') }}">{{ __('List Article') }}</a>
                         </div>
+                        @endcan
                         @can('admin')
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('admin.registered.user') }}">{{ __('Registered User') }}</a>
